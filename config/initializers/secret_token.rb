ef32bba98f531b1aa48f250be03120dd38881c2a
stollefson <stollefson@madisoncollege.edu>
2013-02-24 13:55:05 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookmarcus::Application.config.secret_token = 'a77470d9fda8f3aa809e04fb6695f981b23faf56a4815f5a9143a7cf60744d5a61a4619b026aa2004324177674ddd2eafb5fe50b34bdce39052008fcfa55deb1'
